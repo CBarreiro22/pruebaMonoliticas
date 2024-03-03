24dce14d8dea2575ec5304d74c8bc3e7b2887665
Camilo Barreiro <c.barreiroh@uniandes.edu.co>
2024-03-03 16:25:53 -0500

    Correct dto

--- src/propiedadDeLosAlpes/modulos/propiedades/infraestructura/dto.py
+++ src/propiedadDeLosAlpes/modulos/propiedades/infraestructura/dto.py
@@ -1,17 +1,19 @@
 from propiedadDeLosAlpes.config.db import db
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy import Column, ForeignKey, Integer, Table
 
 import uuid
 
-Base = db.declarative_base()
+Base = declarative_base()
 
-class Propiedad(db.Model):
+
+class Propiedad(Base):
     __tablename__ = "propiedades"
-    id = db.Column(db.String, primary_key=True)
-    direccion = db.Column(db.String)
-    pais :db.Column(db.String)
-    tipo_propiedad:db.Column(db.String)
-    nombre_propietario:db.Column(db.String)
-    fecha_creacion = db.Column(db.DateTime, nullable=False)
-    fecha_actualizacion = db.Column(db.DateTime, nullable=False)
+    id = Column(String, primary_key=True)
+    direccion = Column(String)
+    pais: Column(String)
+    tipo_propiedad: Column(String)
+    nombre_propietario: Column(String)
+    fecha_creacion = Column(DateTime, nullable=False)
+    fecha_actualizacion = Column(DateTime, nullable=False)
