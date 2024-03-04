4361fa4da94d817f4174cbea7a173327de7e476a
Benito Zarate Palomec <b.zarate@uniandes.edu.co>
2024-03-04 10:06:46 -0600

    correcion

--- src/propiedadDeLosAlpes/modulos/propiedades/infraestructura/schema/v1/eventos.py
+++ src/propiedadDeLosAlpes/modulos/propiedades/infraestructura/schema/v1/eventos.py
@@ -1,10 +1,9 @@
 from pulsar.schema import *
 from propiedadDeLosAlpes.seedwork.infraestructura.schema.v1.eventos import EventoIntegracion
-from propiedadDeLosAlpes.seedwork.dominio.eventos import EventoDominio
 
 
 class PropiedadCreadaPayload(Record):
     id_propiedad = String()
 
-class EventoPropiedadCreada(EventoDominio):
+class EventoPropiedadCreada(EventoIntegracion):
     data = PropiedadCreadaPayload()
