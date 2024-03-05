b67e03d7c2030fc2d6e84f61767baa1ee9103247
Beny ZP <b.zarate@uniandes.edu.co>
2024-03-05 00:59:52 +0000

    base de datos

--- src/propiedadDeLosAlpes/config/db.py
+++ src/propiedadDeLosAlpes/config/db.py
@@ -4,9 +4,11 @@ import os
 
 db = None
 
+DB_NAME = os.getenv('DB_NAME', default="propiedades")
 DB_USERNAME = os.getenv('DB_USERNAME', default="user_propiedades")
 DB_PASSWORD = os.getenv('DB_PASSWORD', default="propiedades")
-DB_HOSTNAME = os.getenv('DB_HOSTNAME', default="localhost")
+DB_HOSTNAME = os.getenv('DB_HOSTNAME', default="127.0.0.1")
+DB_PORT = os.getenv('DB_PORT', default="5432")
 
 class DatabaseConfigException(Exception):
     def __init__(self, message='Configuration file is Null or malformed'):
@@ -22,7 +24,7 @@ def database_connection(config, basedir=os.path.abspath(os.path.dirname(__file__
         return f'sqlite:///{os.path.join(basedir, "database.db")}'
     else:
         print('Ingreso')
-        return f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOSTNAME}/propiedades'
+        return f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_NAME}'
 
 def init_db(app: Flask):
     global db
